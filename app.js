ab75bf7c6c3ef465949bb1801ea4e50d6edef273
jigarkataria <jigarkataria1994@gmail.com>
2020-08-31 07:38:37 +0530

    implemented cors

--- app.js
+++ app.js
@@ -17,8 +17,30 @@ var profileRouter = require('./routes/profile')
 const dotenv = require('dotenv');
 dotenv.config();
 const passport = require('passport')
+var cors = require('cors')
 var app = express();
 // app.use(express.urlencoded({extended: true}))
+var allowedOrigins = ['http://someorigin.com',
+                      'http://anotherorigin.com',
+                      'http://localhost:3000'];
+app.use(cors({
+
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+
+  exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+
+  credentials: true,
+}));
 
 
 require('./auth/auth');
